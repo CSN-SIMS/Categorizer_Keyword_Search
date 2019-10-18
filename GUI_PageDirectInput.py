3a77a32dfae74bb393d37c5cc6c032524b534a00
rote1400@student.miun.se <rote1400@student.miun.se>
2019-10-18 09:36:44 +0200

    Improvements: - GUI with three pages (work in progress)

--- GUI_PageDirectInput.py
+++ GUI_PageDirectInput.py
@@ -1,5 +1,5 @@
 from tkinter import Tk, Button, Frame, PhotoImage, Message, Canvas, Label, Listbox, Scrollbar, \
-    RIGHT, X, Y, END, BOTTOM, HORIZONTAL, VERTICAL, Entry, filedialog
+    RIGHT, X, Y, END, BOTTOM, HORIZONTAL, VERTICAL, Entry, Menubutton, Menu, FLAT, OptionMenu, RAISED, filedialog, StringVar
 from keywordSearch import *
 
 
@@ -34,27 +34,64 @@ class PageDirectInput():
         self.img = PhotoImage(file="logo.png")
         self.img = self.img.subsample(6)
         self.logo.create_image(0, 0, anchor='nw', image=self.img)
-        self.var = "Categorization of txt files from a given folder to sub-folders and presenting them in a table"
+        self.var = "Sentiment analysis of a direct input"
         self.infoMessage = Message(self.higher_frame, text=self.var, justify='center', width=350, font=("Courier bold", 14))
         self.infoMessage.place(relx=0.4, rely=0.05, relwidth=0.5, relheight=0.3)
 
-        # Middle frame with info message and button
-        self.middle_frame = Frame(self.root, bg='#FFD164', bd=5)
-        self.middle_frame.place(relx=0.5, rely=0.25, relwidth=0.85, relheight=0.1, anchor='n')
-        self.openFolder = Label(self.middle_frame, text="Language", justify='center', bg='white', fg='#EE7C7D',
-                              font=("Courier", 12,))
-        self.openFolder.place(relx=0, rely=0, relwidth=0.65, relheight=1)
-        self.button = Button(self.middle_frame, text="Browse", font=("Courier", 12), bg='#b3b3b3', activebackground='#f2d9e6',
-                           command=lambda: self.fileDialog())
-        self.button.place(relx=0.7, relwidth=0.3, relheight=1)
+        # Menubar with Option, Result and Direct input
+        self.menubar()
 
         # Lower frame with scrollbars for displaying of categories and file names
         self.lower_frame = Frame(self.root, bg='#FFD164', bd=5)
         self.lower_frame.place(relx=0.5, rely=0.35, relwidth=0.85, relheight=0.55, anchor='n')
         self.lower_frame.grid_rowconfigure(0, weight=1)
         self.lower_frame.grid_columnconfigure(0, weight=1)
-        self.results = Entry(self.lower_frame)
-        self.results.pack()
+        # select language (English or Swedish)
+        self.selectLanguage(self.lower_frame)
+        # Entry text box
+        self.entryLabel = Label(self.lower_frame, text="Enter text", bg='#FFD164', font=("Courier bold", 12))
+        self.entryLabel.place(relx=0.37, rely=0.2, relwidth=0.25, relheight=0.1)
+        self.entryContent = Entry(self.lower_frame, font=("Courier", 12,), justify='center')
+        self.entryContent.place(relx=0, rely=0.3, relwidth=1, relheight=0.6)
+        self.button = Button(self.lower_frame, text="Analyze", font=("Courier", 12), bg='#b3b3b3',
+                             activebackground='#f2d9e6', command=lambda: self.fileDialog())
+        self.button.place(relx=0.4, rely=0.91, relwidth=0.2, relheight=0.1)
+
+    # Middle frame with buttons bar
+    def menubar(self):
+        self.middle_frame = Frame(self.root, bg='#FFD164', bd=5)
+        self.middle_frame.place(relx=0.5, rely=0.25, relwidth=0.85, relheight=0.1, anchor='n')
+        self.button = Button(self.middle_frame, text="Options", font=("Courier", 12), bg='#EE7C7D',
+                             activebackground='#f2d9e6',
+                             command=lambda: self.fileDialog())
+        self.button.place(relx=0.1, relwidth=0.2, relheight=1)
+        self.button = Button(self.middle_frame, text="Result", font=("Courier", 12), bg='#EE7C7D',
+                             activebackground='#f2d9e6',
+                             command=lambda: self.fileDialog())
+        self.button.place(relx=0.4, relwidth=0.2, relheight=1)
+        self.button = Button(self.middle_frame, text="Direct input", font=("Courier", 12), bg='#EE7C7D',
+                             activebackground='#f2d9e6',
+                             command=lambda: self.fileDialog())
+        self.button.place(relx=0.7, relwidth=0.2, relheight=1)
+
+    # select language (English or Swedish)
+    def selectLanguage(self, parentWidget):
+        # Create a Tkinter variable
+        self.tkvar = StringVar()
+        # Dictionary with options
+        self.choices = {'English', 'Swedish'}
+        self.tkvar.set('English')  # set the default option
+        # Popup menu with languages
+        self.popupMenu = OptionMenu(parentWidget, self.tkvar, *self.choices)
+        self.popupLabel = Label(parentWidget, text="Choose a language", font=("Courier bold", 12), bg='#FFD164')
+        self.popupLabel.place(relx=0.1, rely=0, relwidth=0.3, relheight=0.2)
+        self.popupMenu.configure(bd=3, bg='#EE7C7D')
+        self.popupMenu.place(relx=0.5, rely=0, relwidth=0.3, relheight=0.15)
+        # on change dropdown value
+        def change_dropdown(*args):
+            print(self.tkvar.get())
+        # link function to change dropdown
+        self.tkvar.trace('w', change_dropdown)
 
     # Gets the selected folder by the user and uses keywordSearch in txt files, then presents categories and file names
     def fileDialog(self):
